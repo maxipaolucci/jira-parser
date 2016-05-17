e213cb8af72162f4562e6a0b316823ae34e30849
Maxi Paolucci Mac Book <mpaolucci@gmail.com>
2016-05-17 12:49:02 +1200

    pinging jira api

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var app = express();
-var http = require("http");
-var https = require("https");
-var htmlparser = require("htmlparser2");
+var JiraApi = require('jira').JiraApi;
+var fs = require('fs');
+
+
+var config = {
+    protocol: 'https',
+    host: 'jira.vodafone.co.nz',
+    port: 443,
+    user: 'mpaoluc',
+    password: 'Scot1234',
+    apiVersion: '2'
+};
+
+var jira = new JiraApi(config.protocol, config.host, config.port, config.user, config.password, config.apiVersion);
 
 app.use(express.static('public'));
 
@@ -12,60 +23,16 @@ app.listen(port, function () {
 });
 
 
-app.get('/print', function (req, res) {
-    if(req.query) {
-        if(req.query.url === undefined) {
-            res.send({message: "url cannot be undefined"});
-        }
-        var urlPrefix = req.query.url.match(/.*?:\/\//g);
-        req.query.url = req.query.url.replace(/.*?:\/\//g, "");
-        var options = {
-            hostname: req.query.url
-        };
-        if(urlPrefix !== undefined && urlPrefix !== null && urlPrefix[0] === "https://") {
-            options.port = 443;
-            https.get(options, function(result) {
-                processResponse(result);
-            }).on('error', function(e) {
-                res.send({message: e.message});
-            });
-        } else {
-            options.port = 80;
-            http.get(options, function(result) {
-                processResponse(result);
-            }).on('error', function(e) {
-                res.send({message: e.message});
-            });
-        }
-        var processResponse = function(result) {
-            var data = "";
-            result.on("data", function(chunk) {
-                data += chunk;
-            });
-            var tags = [];
-            var tagsCount = {};
-            var tagsWithCount = [];
-            result.on("end", function(chunk) {
-                var parser = new htmlparser.Parser({
-                    onopentag: function(name, attribs) {
-                        if(tags.indexOf(name) === -1) {
-                            tags.push(name);
-                            tagsCount[name] = 1;
-                        } else {
-                            tagsCount[name]++;
-                        }
-                    },
-                    onend: function() {
-                        for(var i = 0; i < tags.length; i++) {
-                            tagsWithCount.push({name: tags[i], count: tagsCount[tags[i]]});
-                        }
-                    }
-                }, {decodeEntities: true});
-                parser.write(data);
-                parser.end();
-                res.send({website: req.query.url, port: options.port, data: data, tags: tagsWithCount});
-            });
-        }
-    }
+app.get('/jira', function (req, res) {
+    jira.findIssue('ATG-5980', function(error, issue) {
+        if (error) throw error;
+        res.json(issue);
+    });
 });
 
+app.get('/mock', function (req, res) {
+    fs.readFile('./mocks/ticket.json', 'utf8', function (error, data){
+        data = JSON.parse(data);
+        res.json(data);
+    });
+});
