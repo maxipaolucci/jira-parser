e78bbbf318df7bd17875dec9f2e4c3f79d037354
Maxi Paolucci <maxipaolucci@gmail.com>
2016-05-25 23:09:11 +1200

    migrated jira services

---
+++ public/js/services/jiraIssueServices.es2015.js
@@ -0,0 +1,59 @@
+class JiraIssueService {
+
+  constructor($http, $q, $timeout) {
+    this.HOST = 'http://localhost:3000';
+    this.$http = $http;
+    this.$q = $q;
+    this.$timeout = $timeout;
+  }
+
+
+  getIssue(username, issueNumber) {
+    let deferred = this.$q.defer();
+    let url = `${this.HOST}/findIssue/${username}/${issueNumber}`;
+
+    //$timeout(() => {
+    this.$http.get(url).success((data) => {
+      deferred.resolve(data);
+    }).error((error) => {
+      deferred.reject(`There was an error trying to retrieve the issue number: ${issueNumber}`);
+    });
+    //}, 2000);
+
+    return deferred.promise;
+  }
+
+  login(user, pass) {
+    let deferred = this.$q.defer();
+    let url = `${this.HOST}/login`;
+    let loginData = {
+      username: user,
+      password: pass
+    };
+
+    //$timeout(() => {
+    this.$http.post(url, loginData).success((data) => {
+      deferred.resolve(data);
+    }).error((error) => {
+      deferred.reject(`There was an error trying login: ${error}`);
+    });
+    //}, 2000);
+
+    return deferred.promise;
+  }
+
+  logout(username) {
+    let deferred = this.$q.defer();
+    let url = `${this.HOST}/logout/${username}`;
+
+    this.$http.get(url).success((data) => {
+      deferred.resolve(data);
+    }).error((error) => {
+      deferred.reject(`There was an error trying to do logout:${error}`);
+    });
+
+    return deferred.promise;
+  }
+}
+
+angular.module('printjira').service('jiraIssueService', JiraIssueService);
--- public/js/services/jiraIssueServices.js
+++ public/js/services/jiraIssueServices.js
@@ -1,50 +1,72 @@
-angular.module('printjira').service('jiraIssueService',function ($http, $q, $timeout) {
+'use strict';
 
-    var host = 'http://localhost:3000/';
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-    this.getIssue = function (username, issueNumber) {
-        var deferred = $q.defer();
-        var url = host + 'findIssue/' + username + '/' + issueNumber;
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-        //$timeout(function() {
-        $http.get(url).success(function(data){
-            deferred.resolve(data);
-        }).error(function(error){
-            deferred.reject('There was an error trying to retrieve the isssue number: ' + issueNumber);
-        });
-        //}, 2000);
+var JiraIssueService = function () {
+  function JiraIssueService($http, $q, $timeout) {
+    _classCallCheck(this, JiraIssueService);
 
-        return deferred.promise;
-    };
+    this.HOST = 'http://localhost:3000';
+    this.$http = $http;
+    this.$q = $q;
+    this.$timeout = $timeout;
+  }
 
-    this.login = function (user, pass) {
-        var deferred = $q.defer();
-        var url = host + 'login';
-        var datas = {
-          username: user,
-          password: pass
-        };
+  _createClass(JiraIssueService, [{
+    key: 'getIssue',
+    value: function getIssue(username, issueNumber) {
+      var deferred = this.$q.defer();
+      var url = this.HOST + '/findIssue/' + username + '/' + issueNumber;
 
-        //$timeout(function() {
-        $http.post(url, datas).success(function(data){
-            deferred.resolve(data);
-        }).error(function(error){
-            deferred.reject('There was an error trying login: ' + error);
-        });
-        //}, 2000);
+      //$timeout(() => {
+      this.$http.get(url).success(function (data) {
+        deferred.resolve(data);
+      }).error(function (error) {
+        deferred.reject('There was an error trying to retrieve the issue number: ' + issueNumber);
+      });
+      //}, 2000);
 
-        return deferred.promise;
-    };
+      return deferred.promise;
+    }
+  }, {
+    key: 'login',
+    value: function login(user, pass) {
+      var deferred = this.$q.defer();
+      var url = this.HOST + '/login';
+      var loginData = {
+        username: user,
+        password: pass
+      };
 
-    this.logout = function (username) {
-        var deferred = $q.defer();
-        var url = host + 'logout/' + username;
+      //$timeout(() => {
+      this.$http.post(url, loginData).success(function (data) {
+        deferred.resolve(data);
+      }).error(function (error) {
+        deferred.reject('There was an error trying login: ' + error);
+      });
+      //}, 2000);
 
-        $http.get(url).success(function(data){
-            deferred.resolve(data);
-        }).error(function(){
-            deferred.reject('There was an error trying to do logout.');
-        });
-        return deferred.promise;
-    };
-});
+      return deferred.promise;
+    }
+  }, {
+    key: 'logout',
+    value: function logout(username) {
+      var deferred = this.$q.defer();
+      var url = this.HOST + '/logout/' + username;
+
+      this.$http.get(url).success(function (data) {
+        deferred.resolve(data);
+      }).error(function (error) {
+        deferred.reject('There was an error trying to do logout:' + error);
+      });
+
+      return deferred.promise;
+    }
+  }]);
+
+  return JiraIssueService;
+}();
+
+angular.module('printjira').service('jiraIssueService', JiraIssueService);
